f9069b244cc5d53ede3b1b9aa114d6a2baf414f0
GabrielBorges2000 <gabriel.vscode@gmail.com>
2023-08-14 08:12:31 -0300

    update env

--- src/env/index.js
+++ src/env/index.js
@@ -12,10 +12,4 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false) {
-  console.error('⚠ Invalid environment variables!', _env.error.format())
-
-  throw new Error('Invalid environment variables!')
-}
-
 export const env = _env.data
